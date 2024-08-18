6ff8923160374a3f9dca543ccb31975c327aa507
Pun VireakRoth <vireakrothpun@gmail.com>
2024-08-18 17:09:59 +0700

    Create a seperate database for test 🧪

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Comment;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $users = User::factory(10)->create();
+
+        $posts = Post::factory(200)
+        ->recycle($users)
+        ->create();
+
+        $comments = Comment::factory(100)
+            ->recycle($users)
+            ->recycle($posts)->create();
+        
+        $vireakroth = User::factory()
+            ->has(Post::factory(45))
+            ->has(Comment::factory(120)->recycle($posts))
+            ->create([
+                'name' => 'PunVireakRoth',
+                'email' => 'example@gmail.com',
+            ]);
+           
+        
+
+        // $vireakroth =  User::factory()->create([
+        //     'name' => 'PunVireakRoth',
+        //     'email' => 'vireakrothpun@gmail.com',
+        // ]);
+    }
+}
